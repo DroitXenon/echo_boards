0feb033890bc860319b87bc457903705a9c9f9c3
DroitXenon <droitinjuste@gmail.com>
2024-03-19 14:36:23 -0400

    Initial Commit

---
+++ echo_boards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  echo_boards
+//
+//  Created by Justin Wang on 2024-03-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ echo_boards/echo_boardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  echo_boardsApp.swift
+//  echo_boards
+//
+//  Created by Justin Wang on 2024-03-19.
+//
+
+import SwiftUI
+
+@main
+struct echo_boardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
